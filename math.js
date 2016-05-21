f1d0e006e6415fa59eacda3b11c9942a299f1a96
Carlos Martin <carlos.martin@columbia.edu>
2016-05-21 13:20:22 -0700

    Added math helper functions to math.js

--- math.js
+++ math.js
@@ -3,5 +3,49 @@ var math = {
 	cos: Math.cos,
 	sin: Math.sin,
 	cosh: Math.cosh,
-	sinh: Math.sinh
+	sinh: Math.sinh,
+	add: function (vector1, vector2)
+	{
+		var result = [];
+		for (var i = 0; i < vector1.length; ++i)
+		{
+			result[i] = vector1[i] + vector2[i];
+		}
+		return result;
+	},
+	subtract: function (vector1, vector2)
+	{
+		var result = [];
+		for (var i = 0; i < vector1.length; ++i)
+		{
+			result[i] = vector1[i] - vector2[i];
+		}
+		return result;
+	},
+	scale: function (vector, scalar)
+	{
+		var result = [];
+		for (var i = 0; i < vector.length; ++i)
+		{
+			result[i] = vector[i] * scalar;
+		}
+		return result;
+	},
+	inner: function (vector1, vector2)
+	{
+		var result = vector1[0] * vector2[0];
+		for (var component = 1; component < vector1.length; ++component)
+		{
+			result -= vector1[component] * vector2[component];
+		}
+		return result;
+	},
+	project: function (vector1, vector2)
+	{
+		return scale(vector2, inner(vector1, vector2));
+	}
 };
+
+
+
+
