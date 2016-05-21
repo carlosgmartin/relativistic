ab48221ff9665dbdb96a1f05ea49b39a6867a142
Carlos Martin <carlos.martin@columbia.edu>
2016-05-21 13:03:25 -0700

    Set up rendering environment with centered view

--- relativistic.js
+++ relativistic.js
@@ -4,10 +4,23 @@ canvas.height = 480;
 document.body.appendChild(canvas);
 var context = canvas.getContext('2d');
 
+var zoom = 10;
+
 function render()
 {
 	context.fillStyle = 'black';
 	context.fillRect(0, 0, canvas.width, canvas.height);
+
+    context.save();
+    context.translate(canvas.width / 2, canvas.height / 2);
+    context.scale(zoom, -zoom);
+
+    context.fillStyle = 'red';
+    context.beginPath();
+    context.arc(0, 0, 1, 0, 2 * Math.PI);
+    context.fill();
+
+    context.restore();
 }
 
 render();
