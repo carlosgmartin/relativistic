049c0a532fcb333215e3eca4b4ca8b6d4af79040
Carlos Martin <carlos.martin@columbia.edu>
2016-05-21 14:23:05 -0700

    Added controls for Lorentz boosts in each direction

--- relativistic.js
+++ relativistic.js
@@ -1,3 +1,5 @@
+var vectors = [];
+
 var canvas = document.createElement('canvas');
 canvas.width = 640;
 canvas.height = 480;
@@ -14,13 +16,12 @@ for (var x = -20; x <= 20; x += 4)
 		objects.push([0, x, y]);
 	}
 }
+vectors = vectors.concat(objects);
 
 
 
 function render()
 {
-	requestAnimationFrame(render);
-
 	context.fillStyle = 'black';
 	context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -42,15 +43,54 @@ render();
 
 /* Zoom in and out using mouse wheel */
 addEventListener('mousewheel', function(event) {
-    //zoom *= Math.exp(-event.wheelDelta / 10000);
+    zoom *= Math.exp(-event.wheelDelta / 10000);
     event.preventDefault();
-
-    for (var i = 0; i < objects.length; ++i)
-	{
-		objects[i] = math.boost(objects[i], [1, 0, 0], [0, 1, 0], event.wheelDelta / 10000);
-	}
 });
 
+var keys = {};
+addEventListener('keydown', function(event) {
+    keys[event.which] = true;
+});
+addEventListener('keyup', function(event) {
+    keys[event.which] = false;
+});
 
+var rapidity = .01;
+setInterval(function() {
+    /* Move left */
+    if (keys[65]) {
+	    for (var i = 0; i < objects.length; ++i)
+		{
+			vectors[i].replace(math.boost(vectors[i], [1, 0, 0], [0, -1, 0], rapidity));
+		}
+    }
+    /* Move right */
+    if (keys[68]) {
+	    for (var i = 0; i < objects.length; ++i)
+		{
+			vectors[i].replace(math.boost(vectors[i], [1, 0, 0], [0, 1, 0], rapidity));
+		}
+    }
+    /* Move up */
+    if (keys[83]) {
+	    for (var i = 0; i < objects.length; ++i)
+		{
+			vectors[i].replace(math.boost(vectors[i], [1, 0, 0], [0, 0, 1], rapidity));
+		}
+    }
+    /* Move down */
+    if (keys[87]) {
+	    for (var i = 0; i < objects.length; ++i)
+		{
+			vectors[i].replace(math.boost(vectors[i], [1, 0, 0], [0, 0, -1], rapidity));
+		}
+    }
+    requestAnimationFrame(render);
+}, 1000/60);
 
-
+// Replace contents of current array with contents of a specified array
+Array.prototype.replace = function (array)
+{
+	this.splice(0);
+	Array.prototype.push.apply(this, array);
+};
